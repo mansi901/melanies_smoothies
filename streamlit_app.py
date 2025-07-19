e8684f281efb4c5212ef2a9ee90b78783ee883de
Mansi Gupta <92874662+mansi901@users.noreply.github.com>
2025-07-20 00:07:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,23 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
 
-
-# Write directly to the app
-st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw: ")
-st.write(
-  """Choose the fruits you want in your custom smoothie.
-  """
-)
+st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom smoothie.")
 
 name_on_order = st.text_input("Name on Smoothie: ", " ")
 st.write("The name on your smoothie will be: ", name_on_order)
 
-# session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-# st.dataframe(data = my_dataframe, use_container_width=True)
-# st.stop()
 
-#Convert the SnowPark Dataframe to a Pandas Dataframe so we can use the LOC function
+# Convert the SnowPark Dataframe to a Pandas Dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
-# Convert Snowpark DataFrame to list for multiselect
-fruit_list = my_dataframe.to_pandas()["FRUIT_NAME"].tolist()
+# Fruit list for multiselect
+fruit_list = pd_df["FRUIT_NAME"].tolist()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -37,18 +26,18 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''
+    ingredients_string = ', '.join(ingredients_list)
+
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-       
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-    # st.write(ingredients_string)
+        try:
+            search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+            fruityvice_response.raise_for_status()
+            fruityvice_data = fruityvice_response.json()
+            st.dataframe(data=fruityvice_data, use_container_width=True)
+        except Exception as e:
+            st.error(f"Failed to fetch data for {fruit_chosen}: {e}")
 
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
                          values ('{ingredients_string}', '{name_on_order}')"""
